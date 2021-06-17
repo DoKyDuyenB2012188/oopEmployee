86374a5010f5701a50f50d6e92bd1a9941b04772
duyen <duyenk200@gmail.com>
2021-06-17 21:50:20 +0700

    oppEmployee

---
+++ oppEmployee/oppEmployee.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+class Employee {
+private:
+    string name;
+    double salary;
+public:
+    Employee() {
+
+    }
+    Employee(string name, double salary) {
+        this->name = name;
+        this->salary = salary;
+    }
+    void setName(string name) {
+        this->name = name;
+    }
+    void setSalary(double salary) {
+        this->salary = salary;
+    }
+    string getName() {
+        return this->name;
+    }
+    double getSalary() {
+        return this->salary;
+    }
+    void display() {
+        cout << "Name: " << this->name << endl;
+        cout << "Salary: " << this->salary << endl;
+    }
+};
+
+class Manager :public Employee {
+private:
+    int bonus;
+public:
+    Manager() {
+
+    }
+    Manager(string name, double salary, int bonus) : Employee(name, salary) {
+        this->bonus = bonus;
+    }
+    void setBonus(int bonus) {
+        this->bonus = bonus;
+    }
+    int getBonus() {
+        return this->bonus;
+    }
+    double getSalary() {
+        return Employee::getSalary() + this->bonus;
+    }
+    void display() {
+        cout << "Name: " << getName() << endl;
+        cout << "Salary: " << getSalary() << endl;
+    }
+};
+
+int main()
+{
+    Manager s1 ("nguyen van a",3600,600);
+    s1.display();
+    return 0;
+}
